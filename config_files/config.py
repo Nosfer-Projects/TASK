1474f731a6fd48dfdffce2a28941d81c5c82fefb
Kosma Wysokiński <114942472+Nosfer-Projects@users.noreply.github.com>
2025-06-12 03:22:32 +0200

    Part 3 (#3)

--- config_files/config.py
+++ config_files/config.py
@@ -1,5 +1,6 @@
 import os
 
-BASE_URL =       "https://www.ing.pl/"
-BROWSER =        BROWSER = os.getenv("BROWSER", "chromium")
+# BASE_URL =       "https://www.ing.pl/"
+BASE_URL =       "https://www.google.com/"
+BROWSER =        os.getenv("BROWSER", "chromium")
 URL_TITLE =      "ING Bank Śląski"
