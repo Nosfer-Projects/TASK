3f9c7c541058205f37f6804a6ae9fc1a51440b34
Kosma <nosfer.python@email.com>
2025-06-12 02:05:20 +0200

    Update

--- config_files/config.py
+++ config_files/config.py
@@ -1,5 +1,5 @@
 import os
 
 BASE_URL =       "https://www.ing.pl/"
-BROWSER =        BROWSER = os.getenv("BROWSER", "chromium")
+BROWSER =        os.getenv("BROWSER", "chromium")
 URL_TITLE =      "ING Bank Śląski"
