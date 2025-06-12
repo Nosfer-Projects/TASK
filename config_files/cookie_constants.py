80008f6bc758b1ab06968ecdf064314a2a541296
Kosma <nosfer.python@email.com>
2025-06-12 07:14:23 +0200

    update config

--- config_files/cookie_constants.py
+++ config_files/cookie_constants.py
@@ -1,3 +1,7 @@
+# This file stores static text constants used in cookie dialogs.
+# These values are used to locate and validate elements like buttons, titles, and descriptions
+# in the cookie consent and customization windows during automated tests.
+
 TITLE_TEXT = "ING Bank Śląski (my) korzysta z cookies i innych podobnych technologii"
 ACCEPT_ALL_BUTTON_TEXT = "Zaakceptuj wszystkie"
 DECLINE_ALL_BUTTON_TEXT = "Odrzuć wszystkie"
